29131774bef9a70b9320a9d933c450280c50f0d9
Nazgul7354 <120272677+Melkor7354@users.noreply.github.com>
2023-11-30 01:21:59 +0530

    first commit

--- backend.py
+++ backend.py
@@ -1,33 +1,58 @@
 import pickle
 import os
+import random
 
-player_base = [['Lionel Messi', 'RW', 93, 'Inter Miami', 'Argentina'], []]
+player_base = [['Lionel Messi', 'RW', 93, 'Inter Miami', 'Argentina']]
 user_path = os.path.expanduser('~')
-path1 = user_path+'/players'
-path2 = user_path+'/squad'
+path_main = user_path+'/football_game'
+path1 = path_main+'/players'
+path2 = path_main+'/squad'
 
 
-def convert_images_to_binary(image_file_paths):
-    binary_cards = []
-    for i in image_file_paths:
-        f = open(i, 'rb')
-        binary_cards.append(f.read())
-        f.close()
-    return binary_cards
-
-
-def check_player_in_squad(players):
-
+def convert_images_to_binary(image_file_path):
+    binary_card = ''
+    f = open(image_file_path, 'rb')
+    binary_card += f.read()
+    f.close()
+    return binary_card
 
 
 def initialize():
-    if os.path.exists(user_path+'/football_game'):
+    if os.path.exists(path_main):
         pass
     else:
         mode = 0o666
+        os.mkdir(path_main, mode=mode)
         os.mkdir(path1, mode=mode)
         os.mkdir(path2, mode=mode)
+        players = []
+        forwards = []
+        defenders = []
+        midfielders = []
+        gk = []
+        for player in player_base:
+            image = convert_images_to_binary(user_path+'/cards/{}.jpeg'.format(player[0]))
+            a = Player(player[0], player[1], player[2], player[3], player[4], image)
+            a.save()
+            players.append(a)
+            if a.position in ('GK'):
+                gk.append(a)
+            elif a.position in ('LB', 'CB', ' RB'):
+                defenders.append(a)
+            if a.position in ('LM', 'RM', 'CM', 'CAM', 'CDM'):
+                midfielders.append(a)
+            else:
+                forwards.append(a)
 
+    def random_initial_squad(forw, mid, defe, goal):
+        s = Squad()
+        f = random.sample(forw, 3)
+        m = random.sample(mid, 3)
+        d = random.sample(defe, 4)
+        g = random.sample(goal, 1)
+        for i in [f, m, d, g]:
+            for j in i:
+                s.append()
 
 
 class Player:
@@ -47,9 +72,42 @@ class Player:
             pickle.dump(self, file)
 
 
-def Squad(list):
+class Squad(list):
     def __init__(self):
         list.__init__(self)
+
+    def replace_player(self, old, new):
+        self.remove(old)
+        self.append(new)
+        self.save()
+
+    def check_player_in_squad(self):
+        forwards = 0
+        midfield = 0
+        defense = 0
+        gk = 0
+        for player in self:
+            if player.position in ('RW', 'LW', 'ST', 'CF'):
+                forwards += 1
+            elif player.position in ('CM', 'CAM', 'CDM', 'RM', 'LM'):
+                midfield += 1
+            elif player.position in ('LB', 'CB', 'RB', 'LWB', 'RWB'):
+                defense += 1
+            else:
+                gk += 1
+        if (forwards > 3) or (forwards < 1) or (midfield > 5) or (midfield < 3) or (defense > 5) or (defense < 3) or \
+                (gk != 1):
+            return False
+        else:
+            return True
+
+    def save(self):
+        with open(path2+'squad', 'wb') as file:
+            pickle.dump(self, file)
+
+
+a = Player('Lionel Messi', 'RW', 93, 'Inter Miami', 'Argentina', 'A')
+a.save()
 
 
 
