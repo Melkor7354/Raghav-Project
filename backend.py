ff5cd05bba9a6140f3d00a3be16015e8c388312d
Nazgul7354 <120272677+Melkor7354@users.noreply.github.com>
2023-11-30 22:41:08 +0530

    first commit

--- backend.py
+++ backend.py
@@ -2,39 +2,18 @@ import pickle
 import os
 import random
 
-player_base = [['LIONEL MESSI','RW', 90],
-['CRISTIANO RONALDO','ST', 86],
-['TONI KROOS','CM',86],
-['GARNACHO','LW',75],
-['HARRY KANE','ST',90],
-['SON','LW',87],
-['COLE PALMER','CAM',66],
-['STERLING','LW',83],
-['ENZO','CM',83],
-['JAMAL MUSIALA','CAM',86],
-['JUDE BELLINGHAM','CM',86],
-['JEREMY DOKU','RW',77],
-['MARCUS RAHSFORD','LW',85],
-['SOFYAN AMRABAT','CM',80],
-['RASMUS HOJLUND','ST',76],
-['PEDRI','CM',86],
-['PABLO GAVI','CB',83],
-['WILLIAM SALIBA','CB',83],
-['DAYOT UPAMECANO','CB',82],
-['KEVIN DE BRUYNE','CAM',91],
-['KARIM BENZEMA','ST',90],
-['JOSHUA KIMMICH','CDM',88],
-['JAN OBLAK','GK',88],
-['ANDREAS CHRISTENSEN', 'CB', 83],
-['ANDREAS PEREIRA','CAM',77],
-['KVARATSKHELIA','LW',86],
-['BASTONI','CB',85],
-['REECE JAMES','RB',84],
-['FERLAND MENDY','LB',82],
-['MIKE MAIGNAN','GK',87],
-['NICHOLAS JACKSON','ST',78],
-['FRED','CDM',81],
-['MALACIA','LB',78], ['RICO LEWIS','RB',73]]
+player_base = [['LIONEL MESSI', 'RW', 90], ['CRISTIANO RONALDO', 'ST', 86],
+               ['TONI KROOS', 'CM', 86], ['GARNACHO', 'LW', 75], ['HARRY KANE', 'ST', 90],
+               ['SON', 'LW', 87], ['COLE PALMER', 'CAM', 66], ['STERLING', 'LW', 83],
+               ['ENZO', 'CM', 83], ['JAMAL MUSIALA', 'CAM', 86], ['JUDE BELLINGHAM', 'CM', 86],
+               ['JEREMY DOKU', 'RW', 77], ['MARCUS RAHSFORD', 'LW', 85], ['SOFYAN AMRABAT', 'CM', 80],
+               ['RASMUS HOJLUND', 'ST', 76], ['PEDRI', 'CM', 86], ['PABLO GAVI', 'CB', 83],
+               ['WILLIAM SALIBA', 'CB', 83], ['DAYOT UPAMECANO', 'CB', 82], ['KEVIN DE BRUYNE', 'CAM', 91],
+               ['KARIM BENZEMA', 'ST', 90], ['JOSHUA KIMMICH', 'CDM', 88], ['JAN OBLAK', 'GK', 88],
+               ['ANDREAS CHRISTENSEN', 'CB', 83], ['ANDREAS PEREIRA', 'CAM', 77], ['KVARATSKHELIA', 'LW', 86],
+               ['BASTONI', 'CB', 85], ['REECE JAMES', 'RB', 84], ['FERLAND MENDY', 'LB', 82],
+               ['MIKE MAIGNAN', 'GK', 87], ['NICHOLAS JACKSON', 'ST', 78], ['FRED', 'CDM', 81],
+               ['MALACIA', 'LB', 78], ['RICO LEWIS', 'RB', 73]]
 user_path = os.path.expanduser('~')
 path_main = user_path+'\\football_game'
 path1 = path_main+'\\players'
@@ -62,30 +41,31 @@ def initialize():
         midfielders = []
         gk = []
         for player in player_base:
-            image = convert_images_to_binary(user_path+'\\cards\\{}.png'.format(player[0]))
+            # image = convert_images_to_binary(user_path+'\\cards\\{}.png'.format(player[0]))
+            image = 'a'
             a = Player(player[0], player[1], player[2], image)
             print(a)
             a.save()
             players.append(a)
-            if a.position in ('GK'):
+            if a.position == 'GK':
                 gk.append(a)
             elif a.position in ('LB', 'CB', ' RB'):
                 defenders.append(a)
-            if a.position in ('LM', 'RM', 'CM', 'CAM', 'CDM'):
+            elif a.position in ('LM', 'RM', 'CM', 'CAM', 'CDM'):
                 midfielders.append(a)
             else:
                 forwards.append(a)
 
-    def random_initial_squad(forw, mid, defe, goal):
-        s = Squad()
-        f = random.sample(forw, 3)
-        m = random.sample(mid, 3)
-        d = random.sample(defe, 4)
-        g = random.sample(goal, 1)
-        for i in [f, m, d, g]:
-            for j in i:
-                s.append(j)
-        s.save()
+        def random_initial_squad(forw, mid, defe, goal):
+            s = Squad()
+            f = random.sample(forw, 3)
+            m = random.sample(mid, 3)
+            d = random.sample(defe, 4)
+            g = random.sample(goal, 1)
+            for i in [f, m, d, g]:
+                for j in i:
+                    s.append(j)
+            s.save()
 
         random_initial_squad(forwards, defenders, midfielders, gk)
 
@@ -106,8 +86,11 @@ class Player:
 
 
 class Squad(list):
-    def __init__(self):
+    def __init__(self, crest=None, name=None):
         list.__init__(self)
+
+        self.crest = crest
+        self.name = name
 
     def replace_player(self, old, new):
         self.remove(old)
@@ -135,13 +118,18 @@ class Squad(list):
             return True
 
     def save(self):
-        with open(path2+'squad', 'wb') as file:
+        with open(path2+'\\main', 'wb') as file:
             pickle.dump(self, file)
 
+
+def read_squad():
+    with open(path2 + '\\main', 'rb') as file:
+        squad = pickle.load(file)
+        players = []
+        for i in squad:
+            players.append((i.name, i.position, i.ovr))
+        print(players)
+
+
 initialize()
-
-
-
-
-
-
+read_squad()
